93e2d4e3216adb92603df2e509588d321b759024
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 14:10:53 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,13 @@
 class Owner
   # code goes here
   @@owners = []
+  @@owners_count = 0
   def initialize(name)
     @@owners << self
     @@owners_count += 1
-
+  end
+  def self.reset
+    @@owners.clear
+    @@owners_count = 0
   end
 end
